03bdc3821dfb4219307a838790353041ecfe20f2
Ed Anisko <ed.anisko@gmail.com>
2023-08-04 15:58:49 -0700

    preparing for move to create-svelte

--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -1,4 +1,4 @@
-import { error, json } from '@sveltejs/kit';
+import { error, json, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const DELETE: RequestHandler = async ({ cookies }) => {
